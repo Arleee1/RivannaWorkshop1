dab08470cd8785a680a67893b76b0b5728af4660
Ethan Ermovick <eeermo@gmail.com>
2022-09-25 12:43:33 -0400

    added files

--- PowerAux/src/main.cpp
+++ PowerAux/src/main.cpp
@@ -9,93 +9,42 @@
 #include <mbed.h>
 #include <rtos.h>
 
-#define LOG_LEVEL          LOG_DEBUG
-#define MAIN_LOOP_PERIOD   1s
-#define ERROR_CHECK_PERIOD 100ms
-#define FLASH_PERIOD       500ms
-
 PowerAuxCANInterface vehicle_can_interface(MAIN_CAN_RX, MAIN_CAN_TX,
                                            MAIN_CAN_STBY);
 BPSCANInterface bps_can_interface(BMS_CAN1_RX, BMS_CAN1_TX, BMS_CAN1_STBY);
 
-bool flashHazards, flashLSignal, flashRSignal = false;
-Thread signalFlashThread;
-
-DigitalOut brake_lights(BRAKE_LIGHT_EN);
 DigitalOut leftTurnSignal(LEFT_TURN_EN);
 DigitalOut rightTurnSignal(RIGHT_TURN_EN);
 
+bool fLeftSignal, fRightSignal = false;
+
 void signalFlashHandler() {
     while (true) {
-        if (flashHazards || flashLSignal || flashRSignal) {
-            if (flashHazards) {
-                bool leftTurnSignalState = leftTurnSignal;
-                leftTurnSignal = !leftTurnSignalState;
-                rightTurnSignal = !leftTurnSignalState;
-            } else if (flashLSignal) {
-                leftTurnSignal = !leftTurnSignal;
-                rightTurnSignal = false;
-            } else if (flashRSignal) {
-                rightTurnSignal = !rightTurnSignal;
-                leftTurnSignal = false;
-            } else {
-                leftTurnSignal = false;
-                rightTurnSignal = false;
-            }
-
-            ThisThread::sleep_for(FLASH_PERIOD);
-        } else {
-            leftTurnSignal = false;
-            rightTurnSignal = false;
+        if (fLeftSignal) {
+            fLeftSignal = true;
         }
-        ThisThread::flags_wait_all(0x1);
+        if (fRightSignal) {
+            fRightSignal = true
+        }
+        ThisThread::sleep_for(2000);
+        fLeftSignal = false;
+        fRightSignal = false;
+        ThisThread::sleep_for
     }
 }
 
-AnalogIn fan_tach(FanTach);
-AnalogIn brake_light_current(BRAKE_LIGHT_CURRENT);
-AnalogIn headlight_current(DRL_CURRENT);
-AnalogIn bms_strobe_current(BMS_STROBE_CURRENT);
-AnalogIn left_turn_current(LEFT_TURN_CURRENT);
-AnalogIn right_turn_current(RIGHT_TURN_CURRENT);
-Thread peripheral_error_thread;
 
-BPSRelayController bps_relay_controller(HORN_EN, DRL_EN, AUX_PLUS,
-                                        BMS_STROBE_EN);
+void PowerAuxCANInterface::handle(ECUPowerAuxCommands *can_struct) {
+    can_struct->log(LOG_INFO);
 
-void peripheral_error_handler() {
-    PowerAuxError msg;
-    while (true) {
-        msg.bps_strobe_error = (bms_strobe_current.read_u16() < 1000 &&
-                                bps_relay_controller.bps_fault_indicator_on());
-        msg.brake_light_error =
-            (brake_light_current.read_u16() < 1000 && brake_lights.read());
-        msg.fan_error = (fan_tach.read_u16() < 1000);
-        msg.left_turn_error =
-            (left_turn_current.read_u16() < 1000 && leftTurnSignal.read());
-        msg.right_turn_error =
-            (right_turn_current.read_u16() < 1000 && rightTurnSignal.read());
-        msg.bps_error = bps_relay_controller.bps_has_fault();
+    brake_lights = can_struct->brake_lights;
 
-        vehicle_can_interface.send(&msg);
-        ThisThread::sleep_for(ERROR_CHECK_PERIOD);
-    }
+    fLeftSignal = can_struct->left_turn_signal;
+    fRightSignal = can_struct->right_turn_signal;
+    //flashHazards = can_struct->hazards;
+
+    signalFlashThread.flags_set(0x1);
 }
-
-int main() {
-    log_set_level(LOG_LEVEL);
-    log_debug("Start main()");
-
-    signalFlashThread.start(signalFlashHandler);
-    peripheral_error_thread.start(peripheral_error_handler);
-
-    while (true) {
-        log_debug("Main thread loop");
-
-        ThisThread::sleep_for(MAIN_LOOP_PERIOD);
-    }
-}
-
 void PowerAuxCANInterface::handle(ECUPowerAuxCommands *can_struct) {
     can_struct->log(LOG_INFO);
 
